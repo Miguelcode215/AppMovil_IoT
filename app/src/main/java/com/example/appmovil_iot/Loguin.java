1cec8a16ccbb87f8a11796f3f93c2edcb7e897a3
Miguel <miguelsilvavelasquez@gmail.com>
2025-03-14 20:28:49 -0500

    primer comit 14/marzo

---
+++ app/src/main/java/com/example/appmovil_iot/Loguin.java
@@ -0,0 +1,37 @@
+package com.example.appmovil_iot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Loguin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_loguin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView Registro = findViewById(R.id.btn_registrar);
+
+        Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registro = new Intent(Loguin.this, Registrarse.class);
+                startActivity(registro);
+            }
+        });
+    }
+}
